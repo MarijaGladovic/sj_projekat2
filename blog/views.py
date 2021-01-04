ba973cb06d8b949a7b932e718ba5c4bd50072609
Marija <marijagladovic@Marijas-MacBook-Pro.local>
2021-01-04 15:55:09 +0100

    Post model kreacija i home funkcija sa templatima

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+
+
+context={}
 
 def home(request):
-	return render (request, "blog/home.html"
-	 )
+	posts = Post.objects.order_by('-date_posted')
+	context['posts'] = posts
+	context['name'] = 'home'
+	return render (request, 'blog/home.html', context)
 
