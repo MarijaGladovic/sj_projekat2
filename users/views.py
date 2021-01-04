9b22017c3a2ffb8626c2a1fa495d6fbeda93c9a2
Marija <marijagladovic@Marijas-MacBook-Pro.local>
2021-01-04 15:14:42 +0100

    profile page and update user info

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm
 from django.contrib import messages
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -13,4 +13,23 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/register.html', {'form': form})   
+
+
+@login_required
+def profile(request):
+	return render(request, 'users/profile.html')
+
+
+
+@login_required
+def update(request):
+	if request.method=='POST':
+		u_form = UserUpdateForm(request.POST, 
+			instance=request.user)	
+		if u_form.is_valid():
+			u_form.save()
+			return redirect('profile')
+	else:
+		u_form = UserUpdateForm(instance=request.user)	
+	return render(request, 'users/update.html', {'u_form':u_form})
